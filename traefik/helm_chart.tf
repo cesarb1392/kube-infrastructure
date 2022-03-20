162c089587f63198d0cfda09bfeae32d5d03d0e2
CésarRB <contact@cesarb.dev>
2022-03-20 14:05:59 +0100

    more

---
+++ traefik/helm_chart.tf
@@ -0,0 +1,39 @@
+resource "kubernetes_namespace" "traefik" {
+  metadata {
+    name = var.namespace
+    labels = {
+      namespace = var.namespace
+    }
+  }
+}
+
+
+resource "helm_release" "traefik" {
+  namespace  = var.namespace
+  name       = "traefik"
+  repository = "https://helm.traefik.io/traefik"
+  chart      = "traefik"
+
+  set {
+    name  = "deployment.replicas"
+    value = 1
+  }
+
+  set {
+    name  = "chart.metadata.name"
+    value = "traefik"
+  }
+
+  values = [
+    data.template_file.ingress_values.rendered
+  ]
+  depends_on = [
+    kubernetes_namespace.traefik
+  ]
+}
+
+data "template_file" "ingress_values" {
+  template = file("traefik/values_cloudflare.yaml")
+}
+
+
